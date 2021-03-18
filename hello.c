6fe471cce4ee1af9ea440e0ffc04ca22b2339ca1
Francisco E. Paez <franpaez+github@gmail.com>
2021-03-17 22:07:36 -0300

    hello world in C.

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+ * Imprime un saludo por la salida estándar.
+ */
+
+#include <stdio.h>      // printf()
+#include <stdlib.h>     // exit()
+
+int main(int argc, char *argv[])
+{
+    // Imprime el saludo en la salida estándar.
+    printf("¡Hola Mundo!\n");
+
+    // Termina la ejecución del proceso.
+    exit(EXIT_SUCCESS);
+}
