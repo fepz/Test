3071e1a0ee39cd902b5560919243ddf1a5f1e245
Francisco E. Paez <franpaez+github@gmail.com>
2021-04-06 10:18:14 -0300

    Add error message to test script.

--- test.sh
+++ test.sh
@@ -4,7 +4,11 @@ expected_result="Hello world!"
 result=$(./hello)
 
 if [ "$result" == "$expected_result" ]; then
+    echo "Ok!"
     exit 0
 else
+    echo "Error!"
+    echo "Result: $result"
+    echo "Expected: $expected_result"
     exit 1
 fi
